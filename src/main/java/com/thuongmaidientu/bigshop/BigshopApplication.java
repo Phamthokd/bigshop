e5796e8c06cfd1591dab3a314646f18b27e652b8
Phamthokd <90166537+Phamthokd@users.noreply.github.com>
2023-10-22 00:02:41 +0700

    Initial commit

---
+++ src/main/java/com/thuongmaidientu/bigshop/BigshopApplication.java
@@ -0,0 +1,13 @@
+package com.thuongmaidientu.bigshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thuongmaidientu/bigshop/BigshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuongmaidientu.bigshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
